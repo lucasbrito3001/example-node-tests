8f23f5db1f10c06508de480dd54d8ec19ff3ec08
Lucas de Brito <lucas.brito3001@gmail.com>
2023-08-15 20:39:09 -0300

    Get pod name # ci: Setting the pode name in the env

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const PORT = 3000;
 
 const app = express();
 
-app.get("/", (req, res) => res.json({ message: "Sample Node" }));
+app.get("/", (req, res) => res.json({ message: "Sample Node", podName: process.env.POD_NAME }));
 app.get("/healthy", (req, res) => res.json({ live: true }));
 
 app.listen(PORT, () => console.log("The server is listening on port: " + PORT));
