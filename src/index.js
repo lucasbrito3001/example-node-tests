ed237ce792d1e1f38126ae01d17eb76458ad9056
Lucas de Brito <lucas.brito3001@gmail.com>
2023-08-15 20:21:09 -0300

    Trigger github jenkins # ci: Testing the github webhook to runs jenkins pipeline

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const PORT = 3001;
+const PORT = 3000;
 
 const app = express();
 
-app.get("/", (req, res) => res.json({ message: "Hello World" }));
+app.get("/", (req, res) => res.json({ message: "Sample Node" }));
 app.get("/healthy", (req, res) => res.json({ live: true }));
 
 app.listen(PORT, () => console.log("The server is listening on port: " + PORT));
